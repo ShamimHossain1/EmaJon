8efa13cc22a46aff90e18b6bdf84247b50c93843
ShamimHossain1 <beastb697@gmail.com>
2023-04-04 23:57:19 +0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-       <h1 className="text-3xl text-center mt-44 font-bold underline">
-      Hello world!
-    </h1>
+      <Header></Header>
+      
     </div>
   )
 }
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+
+const Product = (props) => {
+    //console.log(props)
+    const { img, name, seller, quantity, ratings, price } = props.product
+    // product cart add (onclick function) , rec from shop
+    const handleAddToCart = props.handleAddToCart;
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Ratings: {ratings} stars</p>
+            </div>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to cart <FontAwesomeIcon icon={faShoppingCart} /></button>
+        </div>
+    );
+};
+
+export default Product;
